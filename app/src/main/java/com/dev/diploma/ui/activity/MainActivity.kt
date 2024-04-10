46c17731c5cb1dd6d571802e2d686412f5eb9f99
Denis Shubin <shden6280@gmail.com>
2024-04-10 16:59:27 +0300

    add navigation

---
+++ app/src/main/java/com/dev/diploma/ui/activity/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.dev.diploma.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.dev.diploma.R
+import com.dev.diploma.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController = Navigation.findNavController(this, R.id.fragment_container_view)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
+    }
+}
